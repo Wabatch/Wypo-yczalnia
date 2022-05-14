e133d4e05f5c935fd88d3252ddf1ca47336d5ba0
Andrzej Nowiczenko <t4r4boom@gmail.com>
2022-05-14 15:32:15 +0200

    dodanie klasy klienta, ale nie jest jeszcze skonczona

---
+++ src/com/andrzejnowiczenko/Customer.java
@@ -0,0 +1,35 @@
+package com.andrzejnowiczenko;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Customer {
+
+    //licznik ktory zwieksza sie z kazdym nowym obiektem
+    private static final AtomicInteger count = new AtomicInteger(0);
+    private final int id;
+    private final String name;
+
+    public Customer(String name){
+        this.name = name;
+        this.id = count.incrementAndGet();
+    }
+    public void remove(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/com/andrzejnowiczenko/Vehicle.java
@@ -0,0 +1,5 @@
+package com.andrzejnowiczenko;
+
+public class Vehicle {
+
+}
